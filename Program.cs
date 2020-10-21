2aadb55bfb67383b54fca97a3d89f4c79aa39193
aizydyantyi@gmail.com <aizydyantyi@gmail.com>
2020-10-21 15:11:14 +0200

    I created a project that will allow the user to enter any number and be convercted to word

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Assignment_4
+{
+    class Program
+    {
+    
+        static void Main(string[] args)
+        {
+          Console.WriteLine("Enter a word:");
+          string input = Console.ReadLine();
+           
+          input = input.Replace(" ", string.Empty);
+          
+
+          while (input.Length>0)
+          {
+            int counter = 0;
+              for (int i = 0; i < input.Length; i++)
+              {
+                  if (input[0] == input[i])
+                  {
+                      counter++;
+                  }
+              }
+              Console.WriteLine(input[0]+":" + counter );
+              input = input.Replace(input[0].ToString(), string.Empty); 
+          }
+
+        
+               
+            }
+            
+        }
+
+}
---
+++ wordsCount.cs
@@ -0,0 +1,21 @@
+   
+   public class Word{
+   public static void letters(string word){
+       word = word.Replace(" ", string.Empty);
+	  int counter;
+        //     // Create for loop
+       while (word.Length>0)
+         {
+            counter = 0;
+            for (int i = 0; i < word.Length; i++)
+          {
+               if (word[0] == 
+               
+               word[i])
+               {
+                     counter++;
+              }
+          }
+       }
+        }
+        }
